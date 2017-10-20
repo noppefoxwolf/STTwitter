8ffc3144af3127af15b36ad23a33264daff6b232
🦊Tomoya Hirano <noppelabs@gmail.com>
2017-10-21 03:07:13 +0900

    add carthage proj

---
+++ Carthage/STTwitter/STTwitter/STTwitter.h
@@ -0,0 +1,19 @@
+//
+//  STTwitter.h
+//  STTwitter
+//
+//  Created by Tomoya Hirano on 2017/10/21.
+//  Copyright © 2017年 Tomoya Hirano. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for STTwitter.
+FOUNDATION_EXPORT double STTwitterVersionNumber;
+
+//! Project version string for STTwitter.
+FOUNDATION_EXPORT const unsigned char STTwitterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <STTwitter/PublicHeader.h>
+
+
